21725dfad1c3ec5985050a51f3a6e1f987cc9532
Benjamin-svg166 <bnbussie@gmail.com>
2025-10-23 02:27:56 -0400

    Remove accidental git commands from App.js
    Removed accidental git commands from the file.

--- App.js
+++ App.js
@@ -4,5 +4,9 @@ git push
 git add App.js
 git commit -m "Remove git commands accidentally added to App.js"
 git push
+function BoardRenderer() {
+  return <canvas id="cognition-board" width="800" height="800"></canvas>;
+}
+
 
 
