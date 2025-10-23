f0151ebf765f8cbae819a396a2db735890e07c91
Benjamin-svg166 <bnbussie@gmail.com>
2025-10-23 02:39:14 -0400

    Remove accidental git commands from App.js
    Removed accidental git commands from App.js.

--- App.js
+++ App.js
@@ -1,16 +1,4 @@
-git add App.js
-git commit -m "Remove git commands accidentally added to App.js"
-git push
-git add App.js
-git commit -m "Remove git commands accidentally added to App.js"
-git push
-function BoardRenderer() {
-  return <canvas id="cognition-board" width="800" height="800"></canvas>;
-}
-npm start
-git add .
-git commit -m "Add PieceLayer component for rendering chess pieces"
-git push
+
 
 
 
